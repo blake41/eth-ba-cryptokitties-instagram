b7000831a5ed39a8f23bd9106563b247d484f9e0
Blake Johnson <blake41@gmail.com>
2018-05-26 13:38:00 -0300

    temporary workaround

--- src/CanvasWrapper.js
+++ src/CanvasWrapper.js
@@ -2,47 +2,61 @@ import React, { Component } from 'react'
 import {fabric} from 'fabric';
 class CanvasWrapper extends Component {
 
+  constructor(props) {
+    super(props)
+    this.handleClick = this.handleClick.bind(this)
+    this.state = {compositeImage: null}
+  }
+
   componentDidMount() {
-    // var canvas = new fabric.Canvas('c',{ width: 900, height: 600 });
-    // // canvas.setHeight(1000)
-    // // canvas.setWidth(2000)
-    var props = {
-      width: 1000,
-      height: 1000,
-      left: 50,
-      top: 70,
-      scaleX: .25,
-      scaleY: .25
-    }
-    var puppyProps = {
-      width: 500,
-      height: 500,
-      left: 500,
-      top: 70,
-      scaleX: .25,
-      scaleY: .25
-    }
-    var kittyUrl = this.props.kittySrc
+    var imgURL = 'https://s3.amazonaws.com/eth-kitties/752340.svg';
+
     var canvas = new fabric.Canvas('canvas');
 
-    fabric.Image.fromURL(kittyUrl, function(img) {
-      img.set(props);
-      canvas.add(img).setActiveObject(img);
-    });
+    var pugImg = new Image();
+    pugImg.onload = function (img) {
+        var pug = new fabric.Image(pugImg, {
+            width: 1000,
+            height: 1000,
+            left: 50,
+            top: 70,
+            scaleX: .25,
+            scaleY: .25
+        });
+        canvas.add(pug);
+    };
+    pugImg.src = imgURL;
+    pugImg.crossOrigin = "Anonymous"
 
     fabric.Image.fromURL(this.props.userSrc, function(img) {
-      // img.set(puppyProps);
-      // canvas.add(img).setActiveObject(img);
+  // img.set(puppyProps);
+  // canvas.add(img).setActiveObject(img);
       canvas.setBackgroundImage(img, canvas.renderAll.bind(canvas), {
         scaleX: canvas.width / img.width,
         scaleY: canvas.height / img.height
-     });
+      });
     });
+  }
+
+  handleClick(event) {
+    var canvas = document.getElementById("canvas")
+    // canvas.toBlob((blob) => {
+    // })
+    var dataURL = canvas.toDataURL()
+    this.setState({compositeImage: dataURL})
   }
 
   render() {
     return (
-      <canvas id="canvas" width="640" height="480"></canvas>
+      <div>
+        <canvas id="canvas" width="640" height="480"></canvas>
+        <div>
+          <button onClick={this.handleClick}>Save Composite</button>
+        </div>
+        <div>
+          <img src={this.state.compositeImage}></img>
+        </div>
+      </div>
     );
   }
 }
