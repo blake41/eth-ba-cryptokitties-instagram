a09eafd544cc3586efc39bff7c0e6b541c5b3a02
Alex Singh <alex@alexsingh.com>
2018-05-25 23:18:12 -0300

    Setup static progress bar

--- src/Home.js
+++ src/Home.js
@@ -7,7 +7,13 @@ class Home extends Component {
 
   render() {
     return (
-      <div className="w-100 h-100 flex relative items-center justify-center">
+      <div className="w-100 h-100 flex flex-column relative items-center justify-center">
+        <div className="flex flex-row items-center justify-between">
+          <div className="flex br-100 w1 h1 mh1 bg-near-white shadow-1"></div>
+          <div className="flex br-100 w1 h1 mh1 bg-near-white shadow-1"></div>
+          <div className="flex br-100 w1 h1 mh1 bg-near-white shadow-1"></div>
+          <div className="flex br-100 w1 h1 mh1 bg-near-white shadow-1"></div>
+        </div>
         <CameraContainer />
       </div>
     );
