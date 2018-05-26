ff3eec1a729f1541461cfabf13b797e584dff0aa
Alex Singh <alex@alexsingh.com>
2018-05-26 12:06:51 -0300

    Small root positioning fixes.

--- src/Home.js
+++ src/Home.js
@@ -9,7 +9,7 @@ class Home extends Component {
 
   render() {
     return (
-      <div className="w-100 h-100 flex flex-column relative items-center justify-center">
+      <div className="relative w-100 h-100 flex flex-column relative items-center justify-center">
         <div className="flex flex-row items-center justify-between">
           <div className="flex br-100 w1 h1 mh1 bg-near-white shadow-1"></div>
           <div className="flex br-100 w1 h1 mh1 bg-near-white shadow-1"></div>
