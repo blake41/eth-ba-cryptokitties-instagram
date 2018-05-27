4f49996f2db6324318c49fd707163b227a4620c9
Alex Singh <alex@alexsingh.com>
2018-05-27 02:55:36 -0300

    Updated kittySelect to mirror splash page styling.

--- src/KittySelect.js
+++ src/KittySelect.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
 
 class KittySelect extends Component {
 
@@ -9,39 +11,45 @@ class KittySelect extends Component {
     return (
       <div className="flex flex-column w-100 items-center justify-around">
 
-        <div className="flex w-100 justify-center">
-          <h1 className="f1 tc fw4 ttu uppercase ma0 z-999">Select your Kitty</h1>
+        <div className="flex w-100 justify-center mb3">
+          <h1 className="f1 tc fw4 ttu uppercase ma0 z-999 dark-blue">Select your Kitty</h1>
         </div>
 
-        <div className="flex w-100 flex-row-ns flex-column justify-around">
-          <div className="flex flex-column items-center w-100 mw6 bg-washed-red br1 shadow-hover shadow-3-ns shadow-0 pointer">
-            <img className="dn w-100 mw6" src={this.props.kittySrc}></img>
-            <img className="flex w-100 mw7" src="./images/ck01.svg"></img>
-            <div className="w-100 justify-center">
-              <h3 className="f3 dark-blue tc ttu uppercase mt0">
-                Use Our Kitty
-              </h3>
-              <h3 className="f4 green tc ttu uppercase">
-                (Fastest & Easiest)
-              </h3>
+        <div className="flex w-100 mw8 flex-row-ns flex-column justify-center">
+          <Link className="link w-100" to="/capture">
+            <div className="flex flex-column items-center w-100 mw6 br1 pointer br--left shadow-hover shadow-0  ">
+              <img className="dn w-100 mw6" src={this.props.kittySrc}></img>
+              <img className="flex w-100 mw7" src="./images/ck01.svg"></img>
+              <div className="w-100 justify-center">
+                <h3 className="f3 dark-blue tc ttu uppercase mt0">
+                  Use Our Kitty
+                </h3>
+                <h3 className="f4 green tc ttu uppercase">
+                  (Fastest & Easiest)
+                </h3>
+              </div>
+              <div className="flex w-100 tc justify-content-center ba bw2 f1 dark-blue ph5 pv1 bg-white-50 font-adieu ttu uppercase ">
+                Select
+              </div>
             </div>
-          </div>
-          <div className="flex relative flex-column items-center w-100 mw6 bg-washed-blue br1 shadow-hover shadow-3-ns shadow-0 pointer">
-            <h3 className="absolute f1-ns f3 hot-pink o-80 z-999 rotate45 left0 center-align ttu uppercase">
-              Coming Soon
-            </h3>
-            <img className="dn w-100 mw6" src={this.props.kittySrc}></img>
-            <img className="flex w-100 mw7 o-50" src="./images/ck03.svg"></img>
-            <div className="w-100 justify-center">
-
-              <h3 className="f3 dark-blue tc ttu uppercase mt0">
-                Use Your Kitty
-              </h3>
-              <h3 className="f4 dark-blue tc ttu uppercase">
-                (Import via MetaMask)
-              </h3>
+          </Link>
+          <Link className="link w-100" to='/kittySelect'>
+            <div className="flex flex-column items-center w-100 mw6 br1 br--left">
+              <img className="dn w-100 mw6" src={this.props.kittySrc}></img>
+              <img className="flex w-100 mw7 o-50" src="./images/ck03.svg"></img>
+              <div className="w-100 justify-center">
+                <h3 className="f3 dark-blue tc ttu uppercase mt0">
+                  Use Your Kitty
+                </h3>
+                <h3 className="f4 dark-blue tc ttu uppercase">
+                  (Import via MetaMask)
+                </h3>
+              </div>
+              <div className="flex w-100 tc justify-content-center ba bw2 f1 dark-blue ph5 pv1 bg-white-50 font-adieu ttu uppercase ">
+                Soon
+              </div>
             </div>
-          </div>
+          </Link>
         </div>
       </div>
     );
